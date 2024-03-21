af95bfc91ae33879e6ca79b6d47376141736af02
luffyhaoshoku <luffy3day2years@gmail.com>
2024-03-21 16:12:35 +0000

    test field for buyer domains CRUD

---
+++ estates/src/main/java/com/qa/estates/EstatesApplication.java
@@ -0,0 +1,13 @@
+package com.qa.estates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatesApplication.class, args);
+	}
+
+}
---
+++ estates/src/test/java/com/qa/estates/EstatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.estates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
